22a88424cd26cbb068ff621d3df8c88791cef649
Angelica Contreras <afernandac94@gmail.com>
2024-01-12 13:22:07 -0300

    Ajustes storageEventListener

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { withStorageListener } from './withStorageListener';
+import './ChangeAlert.css';
 
 function ChangeAlert({ show, toggleShow }) {
 	if (show) {
 		return (
-			<div>
-				<p>Hubo Cambios</p>
-				<button onClick={() => toggleShow(false)}>
-					Volver a cargar la información
-				</button>
+			<div className="ChangeAlert-bg">
+				<div className="ChangeAlert-container">
+					<p>
+						Parece que cambiaste tus TODOs en otra pestaña o ventana del
+						navegador.
+					</p>
+					<p>¿Quieres sincronizar tus TODOs?</p>
+					<button
+						className="TodoForm-button TodoForm-button--add"
+						onClick={toggleShow}
+					>
+						Yes!
+					</button>
+				</div>
 			</div>
 		);
 	} else {
